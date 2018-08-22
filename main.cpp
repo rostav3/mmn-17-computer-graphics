2835cd9a654221355e8b268279f230750a97d79f
stav <stav.r.24@gmail.com>
2018-08-22 04:42:14 +0300

    tail animation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <stdlib.h>
-
+#include "Dog.h"
 // misc
 static constexpr GLsizei NUM_COORDINATES = 3;
 
@@ -41,7 +41,7 @@ static GLfloat  u_x = .0;
 static GLfloat  u_y = 1.0;
 static GLfloat  u_z = .0;
 
-
+Dog dog;
 
 
 GLuint loadBMP_custom(const char * imagepath){
@@ -147,7 +147,7 @@ void displayFun() {
     glutSolidCube(1.0);
     glPopMatrix();
 
-    //walls
+//    walls
     glPushMatrix();
     glTranslated(0.4,1,-1.59);
     glScaled(4.0,2.0,0.025);
@@ -172,108 +172,7 @@ void displayFun() {
     glutSolidCube(1.0);
     glPopMatrix();
 
-    glColor3f(0.8,0.5,0.2);
-
-    // tail
-    glPushMatrix();
-    glTranslated(0.25,0.15,0.15);
-    glScaled(0.03,0.03,0.08);
-    glutSolidSphere(1.0, 50, 50);
-    glRotatef(45.0f, 0.0, 1.0, 0.0);
-
-    glPopMatrix();
-
-    // legs
-    glPushMatrix();
-    glTranslated(0.3,0.065,0.3);
-    glScaled(0.03,0.06,0.03);
-    glutSolidSphere(1.0, 50, 50);
-    glRotatef(-90, 40.0, 0.0, 0.0);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslated(0.2,0.065,0.3);
-    glScaled(0.03,0.06,0.03);
-    glutSolidSphere(1.0, 50, 50);
-    glRotatef(-90, 40.0, 0.0, 0.0);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslated(0.2,0.065,0.45);
-    glScaled(0.03,0.06,0.03);
-    glutSolidSphere(1.0, 50, 50);
-    glRotatef(-90, 40.0, 0.0, 0.0);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslated(0.3,0.065,0.45);
-    glScaled(0.03,0.06,0.03);
-    glutSolidSphere(1.0, 50, 50);
-    glRotatef(-90, 40.0, 0.0, 0.0);
-    glPopMatrix();
-
-    // body
-
-    glPushMatrix();
-    glTranslated(0.25,0.15,0.35);
-    glScaled(0.11,0.11,0.2);
-    glutSolidSphere(1.0, 50, 50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslated(0.25,0.25,0.4);
-    glScaled(0.08,0.18,0.08);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
-    // head
-    glPushMatrix();
-    glTranslated(0.25,0.4,0.45);
-    glScaled(0.17,0.155,0.17);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
-
-    glColor3f(0.6,0.4,0.14);
-
-    // hear
-    glPushMatrix();
-    glTranslated(0.36,0.44,0.48);
-    glScaled(0.06,0.09,0.06);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
-    // hear
-    glPushMatrix();
-    glTranslated(0.14,0.44,0.48);
-    glScaled(0.06,0.09,0.06);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
-    // nose
-    glPushMatrix();
-    glTranslated(0.25,0.42,0.6);
-    glScaled(0.02,0.02,0.02);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
-
-    glColor3f(1,1,1);
-
-    // eyes
-    glPushMatrix();
-    glTranslated(0.285,0.45,0.525);
-    glScaled(0.06,0.06,0.06);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
-    // eyes
-    glPushMatrix();
-    glTranslated(0.215,0.45,0.525);
-    glScaled(0.06,0.06,0.06);
-    glutSolidSphere(0.8, 50, 50);
-    glPopMatrix();
-
+    dog.draw();
 
     // ball
     glColor3f(1,0,0);
@@ -370,12 +269,6 @@ void keyboardFun(unsigned char key, int x, int y) {
         case 'q':
             camera_move_z += 0.1;
             break;
-        case 'p':
-            zoom_factor -= 0.1;
-            break;
-        case 'm':
-            zoom_factor += 0.1;
-            break;
         case 'z':
             c_x+=0.1;
             break;
@@ -412,6 +305,19 @@ void keyboardFun(unsigned char key, int x, int y) {
         case 'N':
             u_z-=0.1;
             break;
+        case 'o':
+            dog.set_tailUpAndDown(true);
+            break;
+        case 'p':
+            dog.set_tailUpAndDown(false);
+            break;
+        case 'u':
+            dog.set_tailRightAndLeft(true);
+            break;
+        case 'i':
+            dog.set_tailRightAndLeft(false);
+            break;
+
         default:
             return;    }
 //    setCameraView();
